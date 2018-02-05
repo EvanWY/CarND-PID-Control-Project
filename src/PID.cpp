d91c2cd60645ddfd19c18af69f7355db0c92f466
Yang Wang <yang@1536-laptop.local>
2018-02-05 09:49:16 -0800

    finish algo

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,23 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    p_error = i_error = d_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error = cte + i_error;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+    return p_error * Kp + i_error * Ki + d_error * Kd;
 }
-
